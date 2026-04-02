90536efac532bf3cb50a2ce9d32adc18cc15781f
Harini M <harinimuruges04@gmail.com>
2026-04-02 23:21:23 +0530

    hated into loved

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -9,7 +9,7 @@ const DisplayHome = () => {
     <>
       <Navbar />
       <div className="mb-4">
-        <h1 className="my-5 font-bold text-2xl">Your Hated Playlist</h1>
+        <h1 className="my-5 font-bold text-2xl">Your Loved Playlist</h1>
         <div className="flex overflow-auto">
           {albumsData.map((item, index) => (
             <AlbumItems
